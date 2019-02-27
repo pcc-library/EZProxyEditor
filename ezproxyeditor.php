d5a8739af7fafa888024b9fdd6e3f5e37a1ed3c6
gustavo.lanzas <gustavo.lanzas@gmail.com>
2019-02-26 21:38:22 -0800

    changed layout to use multiple columns. Blocked out edit form.

--- ezproxyeditor.php
+++ ezproxyeditor.php
@@ -12,3 +12,9 @@ use PCC_EPE\Frontend\RenderUI;
 $data = ParseConfigFile::parseConfig();
 
 echo RenderUI::renderSections($data);
+
+//echo '<pre>'.print_r(json_encode($data['sections'], JSON_PRETTY_PRINT),true).'</pre>';
+
+if($_POST) {
+echo '<pre>'.print_r(json_encode($_POST, JSON_PRETTY_PRINT),true).'</pre>';
+}
