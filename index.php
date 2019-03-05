ab3346c0404ffa29d20b9a47f4ee423202dd6a5b
gustavo.lanzas <gustavo.lanzas@gmail.com>
2019-03-05 12:25:28 -0800

    cleaning up index.php

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
 <?php
 
-namespace PCC_EPE;
-
-require __DIR__ . '/vendor/autoload.php';
-
-define( 'PCCEPEPATH', __DIR__.'/' );
-
-use PCC_EPE\Functions\LoadConfig;
-use PCC_EPE\Frontend\RenderUI;
-
-$data = LoadConfig::init();
-
-echo RenderUI::renderSections($data);
-
-//if($_POST) {
-//echo '<pre>'.print_r(json_encode($data, JSON_PRETTY_PRINT),true).'</pre>';
-//};
+require_once 'ezproxyeditor.php';
