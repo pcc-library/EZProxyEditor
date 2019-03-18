e37dc5bc74004d7fb95bd83d16971789b0f2a0f0
gustavo.lanzas <gustavo.lanzas@gmail.com>
2019-03-17 22:32:08 -0700

    cleaning up main file

--- ezproxyeditor.php
+++ ezproxyeditor.php
@@ -39,8 +39,6 @@ $twig = new Environment($loader, [
 
 $twig->addExtension(new DebugExtension());
 
-//$post_data = $_REQUEST['section'];
-
 Config::$twig = $twig;
 Config::$files = $files;
 Config::$post_data = $_REQUEST;
@@ -54,30 +52,9 @@ $router->map('GET',$strSubfolderRoute.'/write','PCC_EPE\Controllers\RouteControl
 
 $router->map('GET',$strSubfolderRoute.'/preview','PCC_EPE\Controllers\RouteController#preview', 'preview');
 
-
-//// Enable debugging
-//phpCAS::setDebug();
-//// Enable verbose error messages. Disable in production!
-//phpCAS::setVerbose(true);
-//// Initialize phpCAS
-//phpCAS::client(CAS_VERSION_2_0, CAS_HOST, CAS_PORT, CAS_CONTEXT);
-//// For production use set the CA certificate that is the issuer of the cert
-//// on the CAS server and uncomment the line below
-//// phpCAS::setCasServerCACert($cas_server_ca_cert_path);
-//// For quick testing you can disable SSL validation of the CAS server.
-//// THIS SETTING IS NOT RECOMMENDED FOR PRODUCTION.
-//// VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL!
-//phpCAS::setNoCasServerValidation();
-//// force CAS authentication
-//phpCAS::forceAuthentication();
-//// at this step, the user has been authenticated by the CAS server
-//// and the user's login name can be read with phpCAS::getUser().
-//// logout if desired
 if (isset($_REQUEST['logout'])) {
     phpCAS::logout();
 }
-
-
 
 $match = $router->match();
 
@@ -97,5 +74,3 @@ if ($match === false) {
         // Throw an exception in debug, send a 500 error in production
     }
 }
-
-?>
